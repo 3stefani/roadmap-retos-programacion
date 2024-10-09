f292b888328325dcde202ca09673e6f31a90cf91
3stefani <116216372+3stefani@users.noreply.github.com>
2024-10-09 21:52:50 +0200

    3stefani.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/3stefani.py
@@ -0,0 +1,39 @@
+# Comentario de una línea 
+# https://www.python.org/
+
+"""
+Esto es un comentario
+en varias líneas
+"""
+
+'''
+Otra manera de escribir
+comentarios
+'''
+
+# Creo una variable
+variable = "Mi_variable"
+
+# Creo una contstante
+CONSTANTE = 3.1415 
+
+# Creo variables que representan los datos primitivos:
+
+# Número entero (int, entero, integer). Los que no tienen decimales.
+numero_entero = 3 
+
+# Número decimales (float)
+my_float = 3.34
+
+# Número booleano (bool). Solo puede ser verdadero o falso.
+my_bool = True
+my_bool = False
+
+# Cadena de texto (str, string)
+my_str = "Con comillas dobles" 
+my_other_string = ' Con comillas simples' 
+
+
+
+# Imprimo por pantalla
+print("¡Hola, Python!")
